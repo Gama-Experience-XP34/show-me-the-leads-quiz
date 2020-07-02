45abd19987b5ae153cf2e91f6c773831f281732b
AndresIba <cristian.ibanez@aluno.unila.edu.br>
2020-07-02 04:19:13 -0300

    Atualizando estilos CSS e estrutura no HTML

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-var cidades = ['New York', 'Orlando', 'Paris', 'Sidney', 'Londres', 'Toronto', 'Agra', 'Rio de Janeiro', 'Porto de Galinhas', 'Maragogi', 'Bonito-MS', 'Capadócia', 'Osasco', 'Chernobyl', 'Cidade do Cabo']
+/* let cities = [
+  { Destino: 'New York', Score: 0},
+  { Destino: 'Orlando', Score: 0},
+  { Destino: 'Paris', Score: 0},
+  { Destino: 'Sidney', Score: 0},
+  { Destino: 'Londres', Score: 0},
+  { Destino: 'Toronto', Score: 0},
+  { Destino: 'Agra', Score: 0},
+  { Destino: 'Rio de Janeiro', Score: 0},
+  { Destino: 'Porto de Galinhas', Score: 0},
+  { Destino: 'Maragogi', Score: 0},
+  { Destino: 'Bonito-MS', Score: 0},
+  { Destino: 'Capadócia', Score: 0},
+  { Destino: 'Osasco', Score: 0},
+  { Destino: 'Chernobyl', Score: 0},
+  { Destino: 'Cidade do Cabo', Score: 0},
+] */
 
-document.querySelectorAll('.btn').forEach(function(e) {
+document.querySelectorAll('.btn-grid').forEach(function (e) {
+  e.addEventListener('click', function () {
+    let value = e.lastElementChild;
+    console.log(value)
+  })
+});
+
+/* document.querySelectorAll('.btn').forEach(function(e) {
     e.addEventListener('click', function() {
       this.style.backgroundColor = "pink";
     })
-  });
+  }); */
 
   
